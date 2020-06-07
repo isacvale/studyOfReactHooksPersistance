476504716dde4c0bd93d2caf1c2abdec6af5f34b
Isac do Vale Oliveira <isac.dvo@gmail.com>
2020-06-07 12:06:16 -0300

    Updates

---
+++ precache-manifest.30ac317953f6e7625642c6796f71a94f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9f31d77df635370f6ae86f68481eda",
+    "url": "/studyOfReactHooksPersistance/index.html"
+  },
+  {
+    "revision": "c892f0ab153797656717",
+    "url": "/studyOfReactHooksPersistance/static/css/main.6b452f9e.chunk.css"
+  },
+  {
+    "revision": "0a3fe688639b05c63ff2",
+    "url": "/studyOfReactHooksPersistance/static/js/2.cb89eccc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/studyOfReactHooksPersistance/static/js/2.cb89eccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c892f0ab153797656717",
+    "url": "/studyOfReactHooksPersistance/static/js/main.cf257bb0.chunk.js"
+  },
+  {
+    "revision": "08c521da1bc3057fc6ac",
+    "url": "/studyOfReactHooksPersistance/static/js/runtime-main.3c4f422d.js"
+  }
+]);
